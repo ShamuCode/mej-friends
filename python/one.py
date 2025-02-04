572a10c29b53a61360108deeb63aaa21696324e8
ShamuLC <120932549+ShamuCode@users.noreply.github.com>
2025-02-04 12:53:51 +0100

    Delete python directory

--- python/one.py
+++
@@ -1,5 +0,0 @@
-from math import *
-
-x = int(input("Number of persons at the party : "))
-result = int((x*x-x)/2)
-print("Number of claps : ", result)
--- python/two.py
+++
@@ -1,6 +0,0 @@
-from math import *
-
-x = int(input("Nombres de personnes à la soiréé : "))
-y = int(input("Nombres de duos detestées (par groupe de 2) : "))
-z = int((x*x-x)/2 - y)
-print("Nombre de clapements : ", z)

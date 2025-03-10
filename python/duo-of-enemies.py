663ff1986ef58c1cf23770216dd17e9f5ded7975
ShamuLC <120932549+ShamuCode@users.noreply.github.com>
2025-03-10 10:37:59 +0100

    Update duo-of-enemies.py

--- python/duo-of-enemies.py
+++ python/duo-of-enemies.py
@@ -1,6 +1,8 @@
+# Created by ShamuLC (ShamuCode)
+
 from math import *
 
 x = int(input("Nombres de personnes à la soiréé : "))
 y = int(input("Nombres de duos detestées (par groupe de 2) : "))
-z = int((x*x-x)/2 - y)
+z = int((x*(x-1))/2 - y)
 print("Nombre de clapements : ", z)

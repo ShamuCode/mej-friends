08bac84e06d6a5d7f9b6b50a07761c23efa5e493
ShamuLC <120932549+ShamuCode@users.noreply.github.com>
2025-03-10 10:38:10 +0100

    Update enemis.py

--- python/enemis.py
+++ python/enemis.py
@@ -1,12 +1,14 @@
+# Created by ShamuLC (ShamuCode)
+
 from math import *
 
-x = int(input("Nombres de personnes à la soiréé : "))
-nb = int(input("Combien de personnes détestent d'autres personnes : "))
+x = int(input("Number of people at the party: "))
+nb = int(input("How many people hate other people: "))
 round = 1
 y = 0
 while nb >= 1:
-    y = y + int(input(f"Nombre de personnes detestés par la personne {round} : "))
+    y = y + int(input(f"Number of people hated by person {round}: "))
     round = round + 1
     nb = nb - 1
-z = int((x*x-x)/2 - y)
-print("Nombre de clapements : ", z)
+z = int((x * (x - 1)) / 2 - y)
+print("Number of claps: ", z)

e385b01f3e472640754239dede970c01779f2b9b
ShamuLC <120932549+ShamuCode@users.noreply.github.com>
2025-03-10 10:38:20 +0100

    Update two-claps.py

--- python/two-claps.py
+++ python/two-claps.py
@@ -1,5 +1,8 @@
+# Created by ShamuLC (ShamuCode)
+
 from math import *
 
-x = int(input("Number of persons at the party : "))
-result = int(((x*x-x)/2)*2)
-print("Number of claps : ", result)
+x = int(input("Number of persons at the party: "))
+y = int(input("Number of claps for each person: "))
+result = int(((x*(x-1))/2)*y)
+print("Number of claps: ", result)

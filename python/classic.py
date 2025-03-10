ebd869d6a328800c5daf0738a6fe723298f48f79
ShamuLC <120932549+ShamuCode@users.noreply.github.com>
2025-03-10 10:37:51 +0100

    Update classic.py

--- python/classic.py
+++ python/classic.py
@@ -1,5 +1,7 @@
+# Created by ShamuLC (ShamuCode)
+
 from math import *
 
 x = int(input("Number of persons at the party : "))
-result = int((x*x-x)/2)
+result = int((x*(x-1))/2)
 print("Number of claps : ", result)
